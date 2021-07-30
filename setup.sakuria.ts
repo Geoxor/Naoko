05e81db3b3d6ade81a0b1be4855d3be0f909dd70
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2021-07-30 16:19:04 +0300

    make sure shit works if they don't have a entry for this at all

--- setup.sakuria.ts
+++ setup.sakuria.ts
@@ -29,6 +29,7 @@ console.log("Icons installed");
 
 // Update the user's VS Code settings to use the new icons for the sakuria files
 const vscodeSettings = require(`C:/Users/${os.userInfo().username}/AppData/Roaming/Code/User/settings.json`);
+vscodeSettings["material-icon-theme.files.associations"] = {};
 vscodeSettings["material-icon-theme.files.associations"]["sakuria.json"] = "../../icons/sakuriaJson";
 vscodeSettings["material-icon-theme.files.associations"]["sakuria.ts"] = "../../icons/sakuria";
 vscodeSettings["material-icon-theme.files.associations"]["*.sakuria"] = "../../icons/sakuria";
