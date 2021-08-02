7e5ed788f4fd609ad65414c3fcdf7867f5bf1fdb
Bluskript <bluskript@gmail.com>
2021-08-02 12:32:00 +0300

    type safety++
    Co-Authored-By: George Tsotsos <34042825+Geoxor@users.noreply.github.com>

--- middleware/filterForCommands.sakuria.ts
+++ middleware/filterForCommands.sakuria.ts
@@ -1,19 +1,19 @@
 import config from "../classes/Config.sakuria";
 import Discord from "discord.js";
 import MessageParser from "../classes/MessageParser.sakuria";
-import { IMessage } from "../types";
+import { IMessage, TExecute } from "../types";
 
 /**
  * returns the function if the message doesn't meet requirements
  * or if the message is by a bot and it parsers the message
  * content and appends the args and command to it
  * @param {Discord.Message} message
- * @param {Function} next
+ * @param {TExecute} next
  * @author Geoxor
  */
-export default function (message: Discord.Message, next: Function): void {
+export default function (message: Discord.Message, next: TExecute): void {
   if (message.content.lastIndexOf(config.prefix) !== 0) return;
-  if (message.channel.type as string === "dm") return;
+  if ((message.channel.type as string) === "dm") return;
   if (message.author.bot) return;
   const { command, args } = new MessageParser(message.content);
   const updatedMessage = message as IMessage;
