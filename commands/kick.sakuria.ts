91efc04614751da1f51ddcebbe37e6b7549fca3f
Bluskript <bluskript@gmail.com>
2021-08-02 12:29:03 +0300

    now it actually kicks
    Co-Authored-By: George Tsotsos <34042825+Geoxor@users.noreply.github.com>

--- commands/kick.sakuria.ts
+++ commands/kick.sakuria.ts
@@ -11,7 +11,10 @@ export default {
     if (!message.member?.permissions.has("KICK_MEMBERS")) return "you don't have perms cunt";
 
     // Kick him
-    db.newKick(message.author.id, targetUser.id);
+    await targetUser.kick();
+
+    // Keep track of the kick
+    await db.newKick(message.author.id, targetUser.id);
 
     // Get fucked
     return `Kicked user <@${targetUser.id}>`;
