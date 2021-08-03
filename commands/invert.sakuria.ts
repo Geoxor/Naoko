688af949f55f960d638f38ae34d574df8fc3429e
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2021-08-03 17:26:37 +0300

    get last attachment & invert urls

--- commands/invert.sakuria.ts
+++ commands/invert.sakuria.ts
@@ -1,6 +1,23 @@
+import axios from "axios";
 import Discord, { MessageOptions } from "discord.js";
 import { invertImage } from "../logic/logic.sakuria";
 import { IMessage } from "../types";
+
+async function getBufferFromMessageUrl(message: IMessage) {
+  // If they are giving us an image url
+  const imageUrl = message.args[0];
+  if (imageUrl) {
+    const response = await axios({method: 'GET', url: imageUrl, responseType: "arraybuffer"});
+    return Buffer.from(response.data);
+  } 
+}
+
+// Last attachment in the current channel
+async function getLastAttachmentInChannel(message: IMessage) {
+  const messages = await message.channel.messages.fetch()
+  const lastMessage = messages.sort((a, b) => b.createdTimestamp - a.createdTimestamp).filter((m) => m.attachments.size > 0).first();
+  return lastMessage?.attachments.first()?.attachment;
+}
 
 export default {
   name: "invert",
@@ -8,7 +25,7 @@ export default {
   requiresProcessing: true,
   execute: async (message: IMessage): Promise<MessageOptions | string> => {
     const attachedImage = message.attachments.first()?.attachment;
-    const targetImage = attachedImage ? attachedImage : message.channel.lastMessage?.attachments.first()?.attachment;
+    const targetImage = await getBufferFromMessageUrl(message) || attachedImage || await getLastAttachmentInChannel(message);
 
     if (!targetImage) return "You didn't provide an image and/or the last message doesn't contain an image";
 
