d8163f500889838d6b47090b8a3e583220f15865
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2021-08-04 03:57:53 +0300

    commented imports

--- commands/index.ts
+++ commands/index.ts
@@ -1,21 +1,31 @@
 import { ICommand } from "../types";
+
+// fun & game commands
 import anime from "./fun/anime.sakuria";
 import ask from "./fun/ask.sakuria";
-import clear from "./moderation/clear.sakuria";
 import decode from "./fun/decode.sakuria";
 import dicksize from "./fun/dicksize.sakuria";
-import help from "./utility/help.sakuria";
-import invite from "./utility/invite.sakuria";
-import kick from "./moderation/kick.sakuria";
 import mors from "./fun/mors.sakuria";
 import say from "./fun/say.sakuria";
 import traceAnime from "./fun/traceAnime.sakuria";
 import uwuify from "./fun/uwuify.sakuria";
+import waifu from "./fun/waifu.sakuria";
+
+// moderation commands
+import clear from "./moderation/clear.sakuria";
+import kick from "./moderation/kick.sakuria";
+
+// utility commands
+import help from "./utility/help.sakuria";
+import invite from "./utility/invite.sakuria";
+
+// image processing commands
 import invert from "./imageProcessing/invert.sakuria";
+
+// music player commands
 import play from "./musicPlayer/play.sakuria";
 import skip from "./musicPlayer/skip.sakuria";
 import shuffle from "./musicPlayer/shuffle.sakuria";
-import waifu from "./fun/waifu.sakuria";
 
 export const commands: ICommand[] = [
   waifu, 
