e0a1075b7ad341fcde9eaf80d6596b88e29cd263
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2021-08-08 17:47:13 +0300

    just give admin to the bot, dealing with perms is pain for UX

--- commands/utility/invite.sakuria.ts
+++ commands/utility/invite.sakuria.ts
@@ -3,6 +3,6 @@ export default {
   description: "Gives you an invite link for sakuria",
   requiresProcessing: false,
   execute: (): string => {
-    return "Oh? You want to have me on your server? Here you go:\nhttps://discord.com/oauth2/authorize?client_id=870496144881492069&permissions=123986897974&scope=bot";
+    return "Oh? You want to have me on your server? Here you go:\nhttps://discord.com/oauth2/authorize?client_id=870496144881492069&permissions=8&scope=bot";
   },
 };
