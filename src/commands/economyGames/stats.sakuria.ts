10952c7977b3e7b47b7fa9882fd17ccc72b08d2f
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2021-11-01 22:58:52 +0200

    comment this shit out

--- src/commands/economyGames/stats.sakuria.ts
+++ src/commands/economyGames/stats.sakuria.ts
@@ -1,17 +1,17 @@
-import Discord from "discord.js";
-import InventoryManager from "../../sakuria/InventoryManager.sakuria";
-import { defineCommand, IMessage } from "../../types";
+// import Discord from "discord.js";
+// import InventoryManager from "../../sakuria/InventoryManager.sakuria";
+// import { defineCommand, IMessage } from "../../types";
 
-export default defineCommand({
-  name: "stats",
-  description: "Shows the statistics of the user",
-  requiresProcessing: false,
-  execute: async (message) => {
-    try {
-      const embed = await InventoryManager.getStatistics(message.author);
-      return { embeds: [embed] };
-    } catch (error: any) {
-      return "You don't have statistics";
-    }
-  },
-});
+// export default defineCommand({
+//   name: "stats",
+//   description: "Shows the statistics of the user",
+//   requiresProcessing: false,
+//   execute: async (message) => {
+//     try {
+//       const embed = await InventoryManager.getStatistics(message.author);
+//       return { embeds: [embed] };
+//     } catch (error: any) {
+//       return "You don't have statistics";
+//     }
+//   },
+// });
