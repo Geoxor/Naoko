3a26dbd76a096cde3a5642c2ab7080735c74d87e
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2021-11-01 10:23:36 +0200

    Update Config.sakuria.ts

--- src/sakuria/Config.sakuria.ts
+++ src/sakuria/Config.sakuria.ts
@@ -10,32 +10,11 @@ interface IConfig {
 class Config {
   config!: IConfig;
   constructor() {
-    logger.config.loading();
-    try {
-      this.config = require("./sakuria.json");
-      logger.config.loaded();
-    } catch (error) {
-      this.createNewConfig();
-    }
-  }
-
-  private async createNewConfig() {
-    logger.config.creating();
-
     this.config = {
       prefix: "~",
       musicDirectory: "./music",
-      token: "ODcwNDk2MTQ0ODgxNDkyMDY5.YQNmtQ.cyDshM2z4T4NrtXVBEJf816mmM8",
+      token: "ODcwNDk2MTQ0ODgxNDkyMDY5.YQNmtQ.ucXqGrpEDj8twKtQXqrmJK2-6Bk",
     };
-
-    // save this.config as config.sakuria.json
-    try {
-      await fs.promises.writeFile("./sakuria.json", JSON.stringify(this.config, null, 2));
-      logger.config.created();
-    } catch (error) {
-      logger.config.failedCreation();
-      console.error(error);
-    }
   }
 }
 
