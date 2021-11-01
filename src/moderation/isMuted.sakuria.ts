76946fb47f9faa8773c4ce92b3e673536409b010
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2021-11-01 21:48:46 +0200

    deleted messages from muted ppl

--- src/moderation/isMuted.sakuria.ts
+++ src/moderation/isMuted.sakuria.ts
@@ -2,6 +2,7 @@ import { IMessage } from "../types";
 
 export function isMuted (message: IMessage) {
   if (message.databaseUser.is_muted || message.member?.roles.cache.has("737011597217628231")){
+    message.delete();
     return true;
   }
   return false;
