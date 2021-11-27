3f75c288870f6dedb4c89a23138736bd3e81db00
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2021-11-28 00:19:51 +0200

    added more constants

--- src/middleware/commandMiddleware.sakuria.ts
+++ src/middleware/commandMiddleware.sakuria.ts
@@ -2,6 +2,7 @@ import config from "../sakuria/Config.sakuria";
 import Discord from "discord.js";
 import MessageParser from "../sakuria/MessageParser.sakuria";
 import { IMessage } from "../types";
+import { GEOXOR_GENERAL_CHANNEL_ID, GEOXOR_ID } from "../constants";
 
 /**
  * Returns the function if the message doesn't meet requirements
@@ -14,7 +15,7 @@ import { IMessage } from "../types";
 export default function (message: Discord.Message, next: (message: IMessage) => any): void {
   if (message.content.lastIndexOf(config.prefix) !== 0) return;
   if (message.author.bot) return;
-  if (message.channelId === "845328432715923487" && message.author.id !== "153274351561605120") return;
+  if (message.channel.id === GEOXOR_GENERAL_CHANNEL_ID && message.author.id !== GEOXOR_ID) return;
   const { command, args } = new MessageParser(message.content);
   const updatedMessage = message as IMessage;
   updatedMessage.command = command.toLowerCase();
--- src/middleware/moderationMiddleware.sakuria.ts
+++ src/middleware/moderationMiddleware.sakuria.ts
@@ -4,14 +4,13 @@ import { isBadWord } from "../moderation/isBadWord.sakuria";
 import { IMessage } from "../types";
 import { isMuted } from "../moderation/isMuted.sakuria";
 import { isIP } from "../moderation/isIP.sakuria";
-import { GEOXOR_GENERAL_CHANNEL_ID, GEOXOR_GUILD_ID, SECRET_GUILD_ID } from "../constants";
+import { GEOXOR_GUILD_ID, SECRET_GUILD_ID } from "../constants";
 
 const checks = [isFreeNitro, isBadWord, isMuted, isIP];
 
 export default function (message: IMessage, next: (message: IMessage) => any): void {
   if (message.author.bot) return next(message);
   if (message.guild?.id !== GEOXOR_GUILD_ID && message.guild?.id !== SECRET_GUILD_ID) return;
-  if (message.channel.id === GEOXOR_GENERAL_CHANNEL_ID) return next(message);
   for (let i = 0; i < checks.length; i++) {
     const checkFn = checks[i];
     const idxString = `[${i + 1}/${checks.length}]`;
