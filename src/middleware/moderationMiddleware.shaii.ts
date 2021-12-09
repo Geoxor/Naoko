5661372392c1a2fa80a14bf2e21c07290f0ac445
qexat <contact@qexat.com>
2021-12-09 15:24:19 +0100

    fix last commit

--- src/middleware/moderationMiddleware.shaii.ts
+++ src/middleware/moderationMiddleware.shaii.ts
@@ -4,14 +4,14 @@ import { isBadWord } from "../moderation/isBadWord.shaii";
 import { IMessage } from "../types";
 import { isMuted } from "../moderation/isMuted.shaii";
 import { isIP } from "../moderation/isIP.shaii";
-import { APPROVED_GUILDS } from "../constants";
+import { GEOXOR_GUILD_ID, SECRET_GUILD_ID, QBOT_DEV_GUILD_ID } from "../constants";
 
 const checks = [isFreeNitro, isBadWord, isMuted, isIP];
 
 export default function (message: IMessage, next: (message: IMessage) => any): void {
   try {
     if (message.author.bot) return next(message);
-    if (message.guild?.id ! in APPROVED_GUILDS) return;
+    if (message.guild?.id !== GEOXOR_GUILD_ID && message.guild?.id !== SECRET_GUILD_ID && message.guild?.id !== QBOT_DEV_GUILD_ID) return;
     for (let i = 0; i < checks.length; i++) {
       const checkFn = checks[i];
       const idxString = `[${i + 1}/${checks.length}]`;
