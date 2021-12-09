e73c59825ef774fe5e197b44cdc7a9f6b985e2a0
Nightmaretopia <86421819+Nightmaretopia@users.noreply.github.com>
2021-12-09 23:52:42 +0800

    new help plz work

--- src/commands/utility/help.shaii.ts
+++ src/commands/utility/help.shaii.ts
@@ -1,42 +1,54 @@
+import config from "../../shaii/Config.shaii";
+import { Collection, GuildMember, Message, MessageEmbed } from "discord.js";
+import { defineCommand, ICommand } from "../../types";
 import shaii from "../../shaii/Shaii.shaii";
-import { defineCommand } from "../../types";
-import { Readable } from "stream";
+
+const categories = ["ECONOMY", "FUN", "IMAGE_PROCESSORS", "MODERATION", "MUSIC", "UTILITY"];
+
+// the keys are categories, the values are command helps
+const categoryCommands = shaii.commands
+    .reduce((acc: Collection<string, ICommand[]>, command: ICommand) => { acc.get(command.category).push(command); return acc; },
+        categories.reduce((acc: Collection<string, ICommand[]>, cur: string) => { acc.set(cur, []); return acc; }, new Collection()));
 
 export default defineCommand({
-  name: "help",
-  category: "UTILITY",
-  aliases: ["h"],
-  description: "The command you just did",
-  requiresProcessing: false,
-  execute: () => {
-    const helpString = [];
+    name: "help",
+    category: "UTILITY",
+    aliases: ["h"],
+    description: "The command you just did",
+    requiresProcessing: false,
+    execute(message: Message) {
+        const categoryName = message.content.match(RegExp(`${config.prefix}${this.name}\s*(?<arg>.*)$`))!.groups!.arg!;
+        const category = categoryCommands.get(categoryName.toUpperCase());
 
-    const categories = ["ECONOMY", "FUN", "IMAGE_PROCESSORS", "MODERATION", "MUSIC", "UTILITY"];
+        const defaultReply = {
+            embeds: [
+                new MessageEmbed({
+                    title: "List of Command Categories",
+                    description: categories
+                        .map(categoryName => `\`${config.prefix}${this.name} ${categoryName.toLowerCase()}\``)
+                        .join("\n"),
+                }),
+            ],
+        };
 
-    for (let i = 0; i < categories.length; i++) {
-      const category = categories[i];
-      const commands = Array.from(shaii.commands.filter((command) => command.category === category));
-
-      let categoryCommands = commands
-        .map((command) => command[1])
-        .map((command) =>
-          command.aliases.length
-            ? `${command.name}: ${command.description} # [${command.aliases.join(", ")}]`
-            : `${command.name}: ${command.description}`
-        )
-        .join("\n");
-
-      helpString.push(`\n# ${category.replace(/_/g, " ")}\n`);
-      helpString.push(`${categoryCommands}\n`);
-    }
-
-    return {
-      files: [
-        {
-          name: "commands.yml",
-          attachment: Readable.from(helpString.join("").trim()),
-        },
-      ],
-    };
-  },
+        return category ? createCategoryHelpMessage(message.member, categoryName.toUpperCase(), category) : defaultReply;
+    },
 });
+
+function createCategoryHelpMessage(member: GuildMember, category: string, commands: ICommand[]): { embeds: MessageEmbed[] } {
+    return {
+        embeds: [
+            new MessageEmbed({
+                title: `Category: ${category.toLowerCase()}`,
+                description: commands.map(command => {
+                    // ✅ means the user can execute the command, ❌ means the user can't execute the command
+                    const emoji = command.permission ? command.permissions.every(member.permissions.has) ? "✅" : "❌" : "✅";
+
+                    return `> ${emoji} **${command.name}** - ${command.description}${'aliases' in command ? `\n  aliases: ${command.aliases.join(', ')}` : ''}`
+                }).join("\n"),
+                footer: { text: "✅ means you can execute the command, ❌ means you can't execute the command" },
+                color: 'LUMINOUS_VIVID_PINK',
+            })
+        ],
+    };
+}
