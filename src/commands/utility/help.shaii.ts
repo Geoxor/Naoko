3be7a3aba331003e2fe9060e8e8b35efbcacea0f
Nightmaretopia <86421819+Nightmaretopia@users.noreply.github.com>
2021-12-16 00:18:56 +0800

    Update help.shaii.ts

--- src/commands/utility/help.shaii.ts
+++ src/commands/utility/help.shaii.ts
@@ -1,6 +1,15 @@
-import shaii from "../../shaii/Shaii.shaii";
+import { MessageEmbed, MessageActionRow, MessageSelectMenu, MessageSelectOptionData } from 'discord.js';
 import { defineCommand } from "../../types";
-import { Readable } from "stream";
+import { COMMAND_CATEGORIES } from "src/constants";
+
+const selectOptions: MessageSelectOptionData[] = COMMAND_CATEGORIES
+  .map(([label, emoji]) => ({
+    label,
+    value: label.toLowerCase(),
+    description: `All ${label.toLowerCase().split('_').join(' ')} commands.`,
+    emoji,
+    default: false,
+  }));
 
 export default defineCommand({
   name: "help",
@@ -9,35 +18,26 @@ export default defineCommand({
   aliases: ["h"],
   description: "The command you just did",
   requiresProcessing: false,
-  execute: () => {
-    const helpString = [];
-
-    const categories = ["ECONOMY", "FUN", "IMAGE_PROCESSORS", "MODERATION", "MUSIC", "UTILITY"];
-
-    for (let i = 0; i < categories.length; i++) {
-      const category = categories[i];
-      const commands = Array.from(shaii.commands.filter((command) => command.category === category));
-
-      let categoryCommands = commands
-        .map((command) => command[1])
-        .map((command) =>
-          command.aliases.length
-            ? `${command.name}: ${command.description} # [${command.aliases.join(", ")}]`
-            : `${command.name}: ${command.description}`
-        )
-        .join("\n");
-
-      helpString.push(`\n# ${category.replace(/_/g, " ")}\n`);
-      helpString.push(`${categoryCommands}\n`);
-    }
-
-    return {
-      files: [
-        {
-          name: "commands.yml",
-          attachment: Readable.from(helpString.join("").trim()),
-        },
+  execute: (message) => {
+    message.channel.send({
+      embeds: [
+        new MessageEmbed({
+          title: 'All Shaii commands',
+          description: 'Please s  elect one command category.',
+          color: 'LUMINOUS_VIVID_PINK',
+        }),
       ],
-    };
+      components: [
+        new MessageActionRow({
+          components: [
+            new MessageSelectMenu({
+              customId: message.author.id,
+              placeholder: '❔ Categories',
+              options: selectOptions,
+            }),
+          ],
+        }),
+      ],
+    });
   },
 });
