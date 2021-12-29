413f74d9c3e716c4c011c6662840971b2f4868d4
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2021-12-29 14:27:33 +0200

    Update isMuted.shaii.ts

--- src/moderation/isMuted.shaii.ts
+++ src/moderation/isMuted.shaii.ts
@@ -2,7 +2,7 @@ import { MUTED_ROLE_ID } from "../constants";
 import { IMessage } from "../types";
 
 export function isMuted(message: IMessage) {
-  if (message.databaseUser.is_muted || message.member?.roles.cache.has(MUTED_ROLE_ID)) {
+  if (message.databaseUser?.is_muted || message.member?.roles.cache.has(MUTED_ROLE_ID)) {
     return true;
   }
   return false;
