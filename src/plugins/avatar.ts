c91ebcf678952ccb117ffa3a4468cc28cd90770e
Qexat <contact@qexat.com>
2022-01-02 14:20:55 +0100

    supports now server avatars

--- src/plugins/avatar.ts
+++ src/plugins/avatar.ts
@@ -1,5 +1,8 @@
 import { definePlugin } from "../shaii/Plugin.shaii";
 import { defineCommand } from "../types";
+import config from "../shaii/Config.shaii";
+
+import Axios from "axios";
 
 export default definePlugin({
   name: "@geoxor/avatar",
@@ -11,9 +14,24 @@ export default definePlugin({
     usage: "avatar <@user | user_id>",
     description: "Get the avatar of a user or yours",
 
-    execute: (message) => {
+    execute: async (message) => {
       const otherUser = message.mentions.users.first() || message.client.users.cache.get(message.args[0]) || message.author;
-      const link = (otherUser ? otherUser.avatarURL() : message.author.avatarURL()) || message.author.defaultAvatarURL;
+      let link;
+
+      if (message.guild) {
+        const req = await Axios.get(`https://discord.com/api/guilds/${message.guild.id}/members/${otherUser.id}`, {
+          headers: {
+            Authorization: `Bot ${config.token}`
+          }
+        });
+
+        if (req.data.avatar) {
+          link = `https://cdn.discordapp.com/guilds/${message.guild.id}/users/${otherUser.id}/avatars/${req.data.avatar}.png`;
+        }
+      }
+
+      if (!link) link = (otherUser ? otherUser.avatarURL() : message.author.avatarURL()) || message.author.defaultAvatarURL;
+
       return link + "?size=2048";
     },
   }),
