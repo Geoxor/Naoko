a3b27e86312259426702fa4fbf4d92cdbc3c01cd
N1kO23 <niko.huuskonen.00@gmail.com>
2022-01-25 09:54:13 +0200

    Created middleware for restricted channels

---
+++ src/middleware/restrictedChannelMiddleware.shaii.ts
@@ -0,0 +1,25 @@
+import { ADMIN_ROLE_ID, GEOXOR_GUILD_ID, GEOXOR_ID, MOD_ROLE_ID, restricedChannels, SVRGE_ID } from "../constants";
+import logger from "../shaii/Logger.shaii";
+import { IMessage } from "../types";
+
+
+/**
+ * A Middleware that checks if the message is in a restricted channel and if it is, checks if the user has the required permissions to utilize bot commands in that channel.
+ * @param message IMessage to be checked
+ * @param next The next function to be called
+ * @return Returns void if checks are not met, otherwise returns the next function with the message
+ */
+export default function (message: IMessage, next: (message: IMessage) => any): void {
+  try {
+    // Not Geoxor's guild or restricted channel, continue to next function
+    if (message.guild?.id !== GEOXOR_GUILD_ID || !restricedChannels.includes(message.channel.id)) 
+      return next(message);
+    // Geoxor's guild and restricted channel, check if user is a mod or admin, svrge or geo, if not, return
+    if (!(message.member?.roles.cache.some(role => role.id === MOD_ROLE_ID || role.id === ADMIN_ROLE_ID) || message.author.id === GEOXOR_ID || message.author.id === SVRGE_ID))
+      return;
+    return next(message);
+  } catch (error) {
+    logger.error(error as string);
+    return;
+  }
+}
