a966b2fa83ddfede76b9655d8d0357ae75bcc6f1
Solomon <solomon.bodrug@gmail.com>
2022-04-14 15:10:39 -0700

    baking expert :bobby:

---
+++ src/plugins/baking.ts
@@ -0,0 +1,31 @@
+import { SLURS } from "../constants";
+import { CAKES, PIES } from "../plugins/baking-const/baking";
+import { randomChoice } from "../logic/logic.shaii";
+import { definePlugin } from "../shaii/Plugin.shaii";
+import { defineCommand } from "../types";
+
+const bake = defineCommand({
+  name: "bake",
+  category: "FUN",
+  usage: "bake <what-to-bake>",
+  description: "Bake something",
+  execute: async (message) => {
+    if (message.args.length === 0) return `What do you want to bake ${randomChoice(SLURS)}`;
+
+    // the baked goods + easter eggs
+    switch (message.args.join(" ").toLowerCase()) {
+      case "cake":
+        return `${randomChoice(CAKES)}`;
+      case "pie":
+        return `${randomChoice(PIES)}`;
+      case "OFC":
+        return "https://cdn.discordapp.com/attachments/963948583806205962/963971146074705920/shit.png";
+    }
+  },
+});
+
+export default definePlugin({
+  name: "@shkoop/baking",
+  version: "1.0.0",
+  command: [bake],
+});
