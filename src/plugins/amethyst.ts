341d6ed5295f9f5c78bc8048b0c052e51501ebc2
otiskujawa <49094075+otiskujawa@users.noreply.github.com>
2022-10-23 18:29:10 +0200

    Update amethyst descryption

--- src/plugins/amethyst.ts
+++ src/plugins/amethyst.ts
@@ -23,11 +23,11 @@ export default definePlugin({
       // default
       embed
         .setTitle("Amethyst")
-        .setURL("https://github.com/amethyst-px/amethyst")
+        .setURL("https://github.com/Geoxor/amethyst")
         .setDescription(
-          "An electron-based audio player with a pixel-art aesthetic"
+          "An electron-based audio player with a node-based audio routing system"
         )
-        .addField("GitHub", "https://github.com/amethyst-px/amethyst")
+        .addField("GitHub", "https://github.com/Geoxor/amethyst")
         .setImage("https://camo.githubusercontent.com/2b8bfd96500c342aeb89839e2f68177a1b556148ddb36d9c64aa991e33601b19/68747470733a2f2f6d656469612e646973636f72646170702e6e65742f6174746163686d656e74732f3834353332383433323731353932333438372f3937383836303439373537373334303932382f6c6f676f2e706e673f77696474683d313932266865696768743d313932");
 
       return { embeds: [embed] };
