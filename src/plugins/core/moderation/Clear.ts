e3b258a45bb5f98eb01015f6fcc1126aa275c83a
Its-treason <39559178+Its-treason@users.noreply.github.com>
2023-07-15 15:10:01 +0200

    Fix clear command
    - Will not try to delete ALL messages anymore
    - Confirm deletion of more then 100 messages

--- src/plugins/core/moderation/Clear.ts
+++ src/plugins/core/moderation/Clear.ts
@@ -1,4 +1,4 @@
-import { TextChannel, DiscordAPIError, codeBlock } from "discord.js";
+import { TextChannel, ComponentType, codeBlock, User, PermissionFlagsBits, ButtonBuilder, ButtonStyle, ActionRowBuilder } from "discord.js";
 import MessageCreatePayload from "../../../pipeline/messageCreate/MessageCreatePayload";
 import { CommandExecuteResponse } from "../../../types";
 import AbstractCommand, { CommandData } from "../../AbstractCommand";
@@ -15,7 +15,17 @@ export class Clear extends AbstractCommand {
       return `⚠️ ${args[0]} is not a valid number!`;
     }
 
-    const channel = message.channel as TextChannel;
+    if (count > 100) {
+      const confirmedClear = await this.confirmDelete(message.author, message.channel as TextChannel, count);
+      if (confirmedClear !== true) {
+        return confirmedClear;
+      }
+    }
+
+    await this.doClear(count, message.channel as TextChannel);
+  }
+
+  private async doClear(count: number, channel: TextChannel) {
     let deletedCount = 0;
     try {
       // Count MUST be between 2 and 100
@@ -23,7 +33,7 @@ export class Clear extends AbstractCommand {
         const countToDelete = count > 100 ? 100 : count;
         const deleteResult = await channel.bulkDelete(countToDelete, true);
         deletedCount += deleteResult.size;
-        if (deleteResult.size === 0) {
+        if (deleteResult.size === 0 || deletedCount >= count) {
           break;
         }
       }
@@ -31,6 +41,55 @@ export class Clear extends AbstractCommand {
     } catch (error: any) {
       await channel.send(`An error occurred while deleting messages:\n${codeBlock(error.message || error)}`);
     }
+  }
+
+  private async confirmDelete(author: User, channel: TextChannel, count: number): Promise<true | string> {
+    let authorMember;
+    try {
+      authorMember = await channel.guild.members.fetch(author);
+    } catch {
+      return 'Internal error: Could not fetch member for permission check';
+    }
+
+    if (!authorMember.permissions.has(PermissionFlagsBits.Administrator)) {
+      return 'You need "Administrator" permission to delete more than 100 Messages at once!';
+    }
+
+    const confirmBtn = new ButtonBuilder()
+      .setCustomId('confirm-clear')
+      .setLabel('Yes, do as I say!')
+      .setEmoji('⚠️')
+      .setStyle(ButtonStyle.Danger);
+    const row = new ActionRowBuilder().addComponents(confirmBtn);
+
+    const confirmMessage = await channel.send({
+      content: `Do you really want to delete the last ${count} messages? (30s Timeout)`,
+      // @ts-expect-error TODO: Fix me
+      components: [row],
+    });
+
+    let executed = false;
+    const collector = confirmMessage.createMessageComponentCollector({ componentType: ComponentType.Button, time: 30_000 });
+    return new Promise((resolve) => {
+      collector.on('collect', async (evt) => {
+        if (evt.user.id !== author.id) {
+          await evt.reply({ content: 'Only the original author can confirm this action!', ephemeral: true });
+          return;
+        }
+        executed = true;
+        resolve(true);
+      });
+      collector.on('end', async () => {
+        if (executed) return;
+
+        confirmBtn.setLabel('Timeout').setEmoji('⌛').setDisabled();
+        const row = new ActionRowBuilder().addComponents(confirmBtn);
+        // @ts-expect-error TODO: Fix me
+        await confirmMessage.edit({ components: [row] });
+
+        resolve('Confirmation timeout!');
+      })
+    })
   }
 
   get commandData(): CommandData {
